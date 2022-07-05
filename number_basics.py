d7e24a1167f3a91d1c3dbbd9e52bfdb931699f0a
1107tomkim <84105893+1107tomkim@users.noreply.github.com>
2022-07-05 18:26:34 -0500

    Basic usage of number with Python print statement

---
+++ number_basics.py
@@ -0,0 +1,58 @@
+# Printing out different types of numbers
+print("Simply printing out numbers of different types")
+print(2) # Whole number
+print(0.214) # Float number
+print(-2) # Negative whole number
+print(-2.15) # Negative float number
+print("____________________________________________________") 
+
+# Basic Arithematics 
+print (10 + 2.5) # Addition
+print (10 - 2) # Subtraction
+print (10 * 2.5) # Multiplication
+print (10 / 2) # Division
+print("____________________________________________________") 
+
+# Complex mathematical equations
+print((10 + 2) * 5)
+print(10 * (2 + 5))
+print("____________________________________________________") 
+
+# Modulus " % "
+# Modulus will print out the remaining value of the division.
+print(10 % 5) 
+print(10 % 4)
+print(10 % 3)
+print(10 % 2)
+print(10 % 1)
+print("The answer of the output should be 0, 2 ,1 ,0 ,0")
+print("____________________________________________________") 
+
+# Storing numbers in a variable
+my_num = 20 # Setting the value of a variable my_num to 20
+print(my_num)
+my_num = 25 # changing the value of my_num and updating it
+print(my_num)
+my_num = 30 # changing it once again for test reasons.
+print(my_num)
+print("____________________________________________________") 
+
+# Changing a number to a string
+my_num = 10
+print(my_num) # In this print statement my_num is a integer
+print(str(my_num)) # In this print statement, my_num is now a string
+print(type(my_num)) # Using the type function to check the variable
+print(type(str(my_num))) # As you can see the variable changes to a string when using the str() function
+print("____________________________________________________") 
+
+# How to print number and string
+my_age = 25
+print("Hello my age is my_age") # This statement will not print out the age variable that is defined above
+# "print("Hello my age is " + my_age)"  This statement is the right idea but will throw you an error because a string and int cannot be printed in the same line unless!
+print("Hello my age is " + str(my_age)) # This changes the int variable to a string variable allowing you to concatenate the two string to print both number and string
+print("____________________________________________________") 
+
+
+
+
+
