cbb122d5ca99fcc4ec6697d731efc30bc856afa9
1107tomkim <84105893+1107tomkim@users.noreply.github.com>
2022-07-06 18:24:13 -0500

    User input via Python

---
+++ Python_Basics/user_inputs.py
@@ -0,0 +1,6 @@
+# You use the input() function to get user inputs
+name = input("Enter your name here: ")
+age = input("Enter your age here: ")
+print(name)
+print(age)
+print("Hello " + name + " You are " + age + " years old! ")
