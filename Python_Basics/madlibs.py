1b5e3520c371d0f39acca00cef322de554259570
1107tomkim <84105893+1107tomkim@users.noreply.github.com>
2022-07-06 23:44:12 -0500

    Creating a madlib with multiple user inputs

---
+++ Python_Basics/madlibs.py
@@ -0,0 +1,9 @@
+# Creating a madlib using multiple different input from users
+
+color = input("Enter a color: ")                    # Inputting the necessary details as a variable to use in our print statement
+item = input("Enter an item that is blue: ")
+someone = input("Enter someone that you love: ")
+
+print("Roses are " + color )                        # Concatenating string to print a string + the variable
+print(item + " are blue")
+print("I love " + someone)
