34f019dba7d81f29b77c58252593a8c790c0eef3
1107tomkim <84105893+1107tomkim@users.noreply.github.com>
2022-07-06 23:37:29 -0500

    Simple calculator for addition with 2 user inputs

---
+++ Python_Calculator/calculator.py
@@ -0,0 +1,6 @@
+# Simple calculator for addition
+num1 = input("Enter a number: ")
+num2 = input("Enter a number: ")
+result = float(num1) + float(num2)
+
+print("The sum of the two numer is: " + str(result))
