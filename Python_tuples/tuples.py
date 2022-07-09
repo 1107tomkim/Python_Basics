4e9f64c9cd67665d92a00c514ce7fec5a42b90c1
1107tomkim <84105893+1107tomkim@users.noreply.github.com>
2022-07-08 23:06:03 -0500

    Updating some more basic tuples

--- Python_tuples/tuples.py
+++ Python_tuples/tuples.py
@@ -1,11 +1,18 @@
 # Tuples are different than lists!
 # Tuples are created in paranthesis
 # Tuples are immutable meaning they can not be changed or modified AT ALL!!
-coordinates = (4,5)
+coordinates = (4, 5, 6)
 print(coordinates)
 
 # Tuples can be access by indentifying certain indexs
-# coordinates = (4,5)
-# indexs are     0 1
+# coordinates = (4, 5, 6)
+# indexs are     0  1  2
 print(coordinates[0])
 print(coordinates[1])
+
+# You can create a list of tuples
+list_tuples = [("Yes", "No"), (1, 5), ("Hello", "Nugget")]
+print(list_tuples)
+print(list_tuples[0])
+print(list_tuples[1])
+print(list_tuples[2])
