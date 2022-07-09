483963cae0c050515e847e5ffd83e8e3a52b51d5
1107tomkim <84105893+1107tomkim@users.noreply.github.com>
2022-07-08 22:26:16 -0500

    Tuple Basics

---
+++ Python_tuples/tuples.py
@@ -0,0 +1,11 @@
+# Tuples are different than lists!
+# Tuples are created in paranthesis
+# Tuples are immutable meaning they can not be changed or modified AT ALL!!
+coordinates = (4,5)
+print(coordinates)
+
+# Tuples can be access by indentifying certain indexs
+# coordinates = (4,5)
+# indexs are     0 1
+print(coordinates[0])
+print(coordinates[1])
