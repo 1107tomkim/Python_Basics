4e223c42470585a0bd4e34872fd85269d7525eaf
1107tomkim <84105893+1107tomkim@users.noreply.github.com>
2022-07-12 23:53:52 -0500

    Python function basics

---
+++ Python_functions/function_basics.py
@@ -0,0 +1,15 @@
+# Creating a simple function
+
+def sayhi():
+    print("Hello User")
+
+# Calling the simple function
+sayhi()
+
+# Creating a function with parameters
+# Parameters are elements that we are providing as input
+def addit(a, b):
+    c = a + b
+    print(c)
+addit(2, 5)
+addit(3, 6)
