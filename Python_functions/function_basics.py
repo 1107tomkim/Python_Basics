df3dec49ef84e4f804eace7b4a6223736babc8c7
1107tomkim <84105893+1107tomkim@users.noreply.github.com>
2022-07-13 22:05:58 -0500

    Adding some more functions

--- Python_functions/function_basics.py
+++ Python_functions/function_basics.py
@@ -6,6 +6,7 @@ def sayhi():
 # Calling the simple function
 sayhi()
 
+
 # Creating a function with parameters
 # Parameters are elements that we are providing as input
 def addit(a, b):
@@ -13,3 +14,11 @@ def addit(a, b):
     print(c)
 addit(2, 5)
 addit(3, 6)
+
+
+# Creating a function with multiple inputs
+def say_hi(name, age):
+    print("Hello " + name + " " + "you are " + age + " years old!")
+
+say_hi("Mike", "10")
+say_hi("Steve", "20")
