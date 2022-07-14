821df4b517974603460d59c9f9e87fb6f7dac1d6
1107tomkim <84105893+1107tomkim@users.noreply.github.com>
2022-07-13 22:48:26 -0500

    using return statement to print out result and calling functions

---
+++ Python_functions/return_statement.py
@@ -0,0 +1,10 @@
+# Return keyword allows Python to return a result from a function
+
+# Simple function that cubes an input number 
+def cube_it(number): 
+    return number * number * number # Using return statement to return the value of the function
+print(cube_it(5)) # Calling the function and printing it.
+
+# Setting a variable = to the function result and printing it
+result = cube_it(4)
+print(result)
