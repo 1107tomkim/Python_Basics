fd153776e19e59d857eccf947717595956ccbcdc
1107tomkim <84105893+1107tomkim@users.noreply.github.com>
2022-07-15 23:18:59 -0500

    Adding some comments for the function

--- if_statement/if_comparison.py
+++ if_statement/if_comparison.py
@@ -1,11 +1,12 @@
 # Comparing numbers using the if statement
 
+# Function to check which 3 number is the highest
 def max_num(num1, num2, num3):
-    if num1 >= num2 and num1 >= num3:
+    if num1 >= num2 and num1 >= num3: # Checks to see if number 1 is bigger or equal to num2 and num3. 
         return num1
-    elif num2 >= num1 and num2 >= num3:
+    elif num2 >= num1 and num2 >= num3: # Checks to see if number 2 is bigger or equal to num1 and num2.
         return num2
-    else:
+    else: # If none of those conditions above are true then print num3 as the max value.
         return num3
 
 
