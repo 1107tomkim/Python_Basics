f5ec8ecf19f022f3ce8db3947832a313bcd4056f
1107tomkim <84105893+1107tomkim@users.noreply.github.com>
2022-07-15 23:36:46 -0500

    Adding functions and examples for all Arithmetic Operators in python

--- if_statement/operator.py
+++ if_statement/operator.py
@@ -14,10 +14,29 @@ def subtraction(num1, num2):
 print(subtraction(10, 5))
 
 # *   -> Multiplication
+def multiplication(num1, num2):
+    return num1 * num2
+print(multiplication(2, 2))
+
 # /   -> Division
+def division(num1, num2):
+    return num1 / num2
+print(division(10, 2))
+
 # %   -> Modulus
+def modulus(num1, num2):
+    return num1 % num2
+print(modulus(10, 3))
+
 # **  -> Exponentiation
+def exponentiation(num1, num2):
+    return num1 ** num2
+print(exponentiation(5, 2))
+
 # //  -> Floor Division
+def floor_division(num1, num2):
+    return num1 // num2
+print(floor_division(5, 2))
 
 
 #Assignment Operators
