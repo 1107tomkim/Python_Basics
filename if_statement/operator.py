0b36f3dc57e6ab9f1561e19c6845f7a93df383d8
1107tomkim <84105893+1107tomkim@users.noreply.github.com>
2022-07-15 23:41:40 -0500

    Making additional changes to the print statement to make it easily readable results

--- if_statement/operator.py
+++ if_statement/operator.py
@@ -2,41 +2,41 @@
 # Please feel free to add more operator as I may forget some.
 
 
-#Arithmetic Operators
+# Arithmetic Operators
 # +   -> Addition
 def addition(num1, num2):
     return num1 + num2
-print(addition(1, 5))
+print("Solution to addition function when num1 = 1 and num2 = 5 is: " + str(addition(1,5)))
 
 # -   -> Subtraction
 def subtraction(num1, num2):
     return num1 - num2
-print(subtraction(10, 5))
+print("Solution to subtraction function when num1 = 10 and num2 = 5 is: " + str(subtraction(10, 5)))
 
 # *   -> Multiplication
 def multiplication(num1, num2):
     return num1 * num2
-print(multiplication(2, 2))
+print("Solution to multiplication function when num1 = 2 and num2 = 2 is: " + str(multiplication(2, 2)))
 
 # /   -> Division
 def division(num1, num2):
     return num1 / num2
-print(division(10, 2))
+print("Solution to division function when num1 = 10 and num2 = 2 is: " + str(division(10, 2)))
 
 # %   -> Modulus
 def modulus(num1, num2):
     return num1 % num2
-print(modulus(10, 3))
+print("Solution to modulus function when num1 = 10 and num2 = 3 is: " + str(modulus(10, 3)))
 
 # **  -> Exponentiation
 def exponentiation(num1, num2):
     return num1 ** num2
-print(exponentiation(5, 2))
+print("Solution to exponentiation function when num1 = 5 and num2 = 2 is: " + str(exponentiation(5, 2)))
 
 # //  -> Floor Division
 def floor_division(num1, num2):
     return num1 // num2
-print(floor_division(5, 2))
+print("Solution to floor division function when num1 = 5 and num2 = 2 is: " + str(floor_division(5, 2)))
 
 
 #Assignment Operators
