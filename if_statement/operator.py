f9cee112cbccb089aa06c9c9b786ca2ebc4c6c61
1107tomkim <84105893+1107tomkim@users.noreply.github.com>
2022-07-15 23:33:05 -0500

    Changing file from .txt to .py and adding examples to each operator

---
+++ if_statement/operator.py
@@ -0,0 +1,28 @@
+# This file is simply to put all the operators that are allowed in Python.
+# Please feel free to add more operator as I may forget some.
+
+
+#Arithmetic Operators
+# +   -> Addition
+def addition(num1, num2):
+    return num1 + num2
+print(addition(1, 5))
+
+# -   -> Subtraction
+def subtraction(num1, num2):
+    return num1 - num2
+print(subtraction(10, 5))
+
+# *   -> Multiplication
+# /   -> Division
+# %   -> Modulus
+# **  -> Exponentiation
+# //  -> Floor Division
+
+
+#Assignment Operators
+# =   -> Setting something equal to something. EX -> x = 5
+# +=  -> Adding 
+# -+  ->
+# *=  ->
+# /=  ->
