f883adc488f35af2fc8d39d46c90130b164d7e65
1107tomkim <84105893+1107tomkim@users.noreply.github.com>
2022-07-15 22:31:50 -0500

    Added a new example of and operator passing and failing

--- if_statement/if_else_and.py
+++ if_statement/if_else_and.py
@@ -2,7 +2,7 @@
 # for the function to pass the if statement, else it will do what the "else" statement has.
 
 
-# Stating two boolean statement to test
+# Stating two boolean statement to test with one being true and one being false
 am_female = False
 am_male = True
 
@@ -10,3 +10,17 @@ if am_female and am_male: # This "if" statement test for two things, 1 -> if I a
     print("I am both female and a male!") # If both conditions are true then it will print this statement
 else:
     print("I am not both male and female") # It will print this statement if the previous conditions are not true.
+# This function will print the statement "I am not both male and female" because one or more of the statement in the if statement is false
+
+
+
+# Stating two boolean statement to test with both being true
+am_male = True
+am_tall = True
+
+
+if am_male and am_tall: # Statement to check if I am a male and I am tall
+    print("I am a male and I am tall!") # If both statements are true then it will print this statement
+else:
+    print("I dont know what I am!") # Else it will print this statement.
+# This function will print the statement "I am a male and I am tall!" because both statements are true!
