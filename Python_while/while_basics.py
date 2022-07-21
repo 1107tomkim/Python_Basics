1998250183611811e88046a9bbd5aeeb1691bf10
1107tomkim <84105893+1107tomkim@users.noreply.github.com>
2022-07-20 23:43:25 -0500

    Syntax fix

--- Python_while/while_basics.py
+++ Python_while/while_basics.py
@@ -3,6 +3,7 @@
 i = 1
 # The while condition iterates throughout the time the value of i is less than or equal to 10
 while i <= 10:
-    print(i) # Printing the number for visual
-    i += 1 # Incrementing i value by 1, if not it will be an infinite loop
-print("Dont with loop")
+    print(i)    # Printing the number for visual
+    i += 1      # Incrementing i value by 1, if not it will be an infinite loop
+print("Done with loop")
+
