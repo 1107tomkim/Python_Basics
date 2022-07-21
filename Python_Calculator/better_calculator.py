64fccd807e4ad6f252615c09027e14a41dee0ff0
1107tomkim <84105893+1107tomkim@users.noreply.github.com>
2022-07-20 22:37:13 -0500

    Creating a more complex calculator

---
+++ Python_Calculator/better_calculator.py
@@ -0,0 +1,18 @@
+# Creating a more complex calculator
+import math
+
+num1 = float(input("Enter first number: "))
+op = input("Enter the operator you want to use: ")
+num2 = float(input("Enter the second number: "))
+
+if op == "+":
+    print(num1 + num2)
+elif op =="-":
+    print(num1 - num2)
+elif op == "*":
+    print(num1 * num2)
+elif op == "/":
+    print(num1 / num2)
+else:
+    print("Operator not found")
+    
