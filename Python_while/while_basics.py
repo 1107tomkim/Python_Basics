e81a1d1bc86dd55b9fa4771be90feda9c4b16fd2
1107tomkim <84105893+1107tomkim@users.noreply.github.com>
2022-07-20 23:42:33 -0500

    Adding some comments and a print line to show end of loop

--- Python_while/while_basics.py
+++ Python_while/while_basics.py
@@ -1,6 +1,8 @@
 # While loop is used for iteration when certain conditions are true.
 
 i = 1
+# The while condition iterates throughout the time the value of i is less than or equal to 10
 while i <= 10:
-    print(i)
-    i += 1
+    print(i) # Printing the number for visual
+    i += 1 # Incrementing i value by 1, if not it will be an infinite loop
+print("Dont with loop")
