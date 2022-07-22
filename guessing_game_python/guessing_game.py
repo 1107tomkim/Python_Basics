af724d7e560e4158fc50b5bcce53a2c2fc2c1ba1
1107tomkim <84105893+1107tomkim@users.noreply.github.com>
2022-07-21 23:31:27 -0500

    adding more complexity to the guessing game by adding few variables and conditions

--- guessing_game_python/guessing_game.py
+++ guessing_game_python/guessing_game.py
@@ -1,9 +1,20 @@
 # Creating a guessing game using different basic tools we learned in Python so far
 secret_word = "Tommy"
 guess = ""
+count = 0
+count_limit = 3
+out_of_guesses = False
 
+while guess != secret_word and not(out_of_guesses):
+    if count < count_limit:
+        guess = input("What is the secret word: ")
+        print("That is the wrong answer, try again!")
+        count += 1
+    else:
+        out_of_guesses = True
+        print("You are out of guesses")
 
-while guess != secret_word:
-    guess = input("What is the secret word: ")
-    print("That is the wrong answer, try again!")
-print("Good job!")
+if out_of_guesses:
+    print("Nice try but no cookie!")
+else:
+    print("You win!")
