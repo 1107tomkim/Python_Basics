e829c980db1eac32cda3bc66b3361a2a1ed7c240
1107tomkim <84105893+1107tomkim@users.noreply.github.com>
2022-07-25 23:59:53 -0500

    Error showing

--- Python_for_loops/for_loop_basic.py
+++ Python_for_loops/for_loop_basic.py
@@ -5,3 +5,6 @@ players = "I am a player!" # This is what we are going to iterate through
 # Basic for loop
 for player in players: # player is not declared and it can be called whatever you prefer
     print(player) # Will return the whole string thats been printed out character at a time
+
+for anything in players:
+    print(player)
