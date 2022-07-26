e0398ddacb8ed5923b774affa3abc049ede13139
1107tomkim <84105893+1107tomkim@users.noreply.github.com>
2022-07-25 23:59:20 -0500

    More comments

--- Python_for_loops/for_loop_basic.py
+++ Python_for_loops/for_loop_basic.py
@@ -4,4 +4,4 @@ players = "I am a player!" # This is what we are going to iterate through
 
 # Basic for loop
 for player in players: # player is not declared and it can be called whatever you prefer
-    print(player)
+    print(player) # Will return the whole string thats been printed out character at a time
