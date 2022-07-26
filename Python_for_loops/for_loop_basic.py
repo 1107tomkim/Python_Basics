f1ca9112d26d9823e1aa1bb3a6dd9e81802b55e4
1107tomkim <84105893+1107tomkim@users.noreply.github.com>
2022-07-25 23:58:59 -0500

    Adding comments

--- Python_for_loops/for_loop_basic.py
+++ Python_for_loops/for_loop_basic.py
@@ -3,5 +3,5 @@
 players = "I am a player!" # This is what we are going to iterate through
 
 # Basic for loop
-for player in players:
+for player in players: # player is not declared and it can be called whatever you prefer
     print(player)
