5bad7cc951953347439b6f6e19ab8f475330c1a4
1107tomkim <84105893+1107tomkim@users.noreply.github.com>
2022-07-25 23:58:17 -0500

    Adding comment

--- Python_for_loops/for_loop_basic.py
+++ Python_for_loops/for_loop_basic.py
@@ -1,5 +1,6 @@
 # For loop is used to loop through a number of times
-players = "I am a player!"
+
+players = "I am a player!" # This is what we are going to iterate through
 
 for player in players:
     print(player)
